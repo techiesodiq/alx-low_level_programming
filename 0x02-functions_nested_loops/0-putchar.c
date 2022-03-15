4f05b7fc63422975497bc48aef3cad89554846d4
techiesodiq <sodiq.akanmu001@gmail.com>
2022-03-15 15:37:26 -0700

    First commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- Entry
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char word[9] = "_putchar";
+
+	int i = 0;
+
+
+	while (i < 8)
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
